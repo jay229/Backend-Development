0f4a9c51f6a0f033e8ac878b3c855cadb58c4a1f
MRITUNJAY <mritunjaykumartiwari1@gmail.com>
2024-02-12 15:34:36 +0530

    autowiring using byName

---
+++ 03_Spring framework/springcore_basic/src/main/java/com/springcore/autowiring/A.java
@@ -0,0 +1,23 @@
+package com.springcore.autowiring;
+
+public class A {
+	private B b;
+	public A() {
+		super();
+		// TODO Auto-generated constructor stub
+		System.out.println("A is created");
+	}
+	public B getB() {
+		return b;
+	}
+	public void setB(B b) {
+		this.b = b;
+	}
+	public void print() {
+		System.out.println("Hello A");
+	}
+	public void display() {
+		print();
+		b.print();
+	}
+}
---
+++ 03_Spring framework/springcore_basic/src/main/java/com/springcore/autowiring/B.java
@@ -0,0 +1,13 @@
+package com.springcore.autowiring;
+
+public class B {
+
+	public B() {
+		super();
+		// TODO Auto-generated constructor stub
+		System.out.println("B is created");
+	}
+	public void print() {
+		System.out.println("Hello B");
+	}
+}
